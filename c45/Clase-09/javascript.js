6173149e88550244a700b91350f7dfe1126d1a8c
Erik Salinas <salinas.erik2002@gmail.com>
2023-10-17 13:02:35 -0300

    Introdcción a la programación (javascript)

---
+++ c45/Clase-09/javascript.js
@@ -0,0 +1,24 @@
+//*VARIABLES
+var edad = 25; //number /numeros
+var edad = 30;
+
+let nombre = "Maria"; //string / texto
+nombre = "Lucas";
+nombre = "juan";
+let nombreApellido = "Marta Torres";
+const vacaciones = true; //booleano / V o F
+/* const vacaciones = true; */ //!MAALA PRACTICA
+/*  vacaciones = false; */ //!MALA PRACTICA
+
+
+//*CONSOLA
+ console.log(edad);  
+console.log(nombre);
+console.log(vacaciones);
+
+//* PROMPT, ALERT Y CONFIRM 
+/*  prompt("INGRESE SU NOMBRE"); */  /* Muestras un cuadro de diálogo */
+/* alert("ERROR AL INGRESAR USUARIO"); */ /* Muestra un diálogo de alerta */
+/* confirm("¿Desea recibir notificaciónes?") */ /* Muestra un cuadro con dialogo y opciones */
+
+//*CONDICIONALES
